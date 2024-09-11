189a4d00ff7abd1e6d05070ddf16d3ff9b8ad3b4
Edwin Boon <edwin.boon@eo.nl>
2024-09-11 20:52:10 +0200

    Add method to print the book content to the console

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("hello world")
+def main():
+    book_path = "./books/frankenstein.txt"
+    book_content = get_book_content(book_path)
+    print(book_content)
+
+
+def get_book_content(path):
+    with open(path) as f:
+        return f.read()
+
+
+main()
