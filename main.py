43d53b790118efe1ebc5b9a2946e820a4caf36fe
Edwin Boon <edwin.boon@eo.nl>
2024-09-11 21:03:24 +0200

    Print dict with a count for all chars in a given book

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ def main():
     book_content = get_book_content(book_path)
     word_count = count_words(book_content)
     char_count = count_chars(book_content)
+    print(f"Word count: {word_count}")
+    print(f"Character count: {char_count}")
 
 
 def get_book_content(path):
@@ -15,8 +17,14 @@ def count_words(content):
 
 
 def count_chars(content):
-    for char in content:
-        print(char)
+    chars_count = {}
+    for char in content.lower():
+        if char in chars_count:
+            chars_count[char] += 1
+        else:
+            chars_count[char] = 1
+
+    return chars_count
 
 
 main()
